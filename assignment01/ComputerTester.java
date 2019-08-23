6129a07b5b59d9bcee6c4347726b5c39aaf8f9de
Kieran Gaffney <kgaffne1@binghamton.edu>
2019-08-23 10:50:23 -0400

    Tester

---
+++ assignment01/ComputerTester.java
@@ -0,0 +1,29 @@
+package assignment01;
+import java.io.*;
+public class ComputerTester {
+	public static void main(String[] args) {
+	//TODO make 4 different Computer objects with different 
+	//combinations of disk size (two more than a terabyte and two less)
+	//and for each make one an SSD drive and the other not.
+	//Then call System.out.println(comp); for each "comp" object
+	//and observe that there are small differences in how the computer
+	//information prints out.
+		Computer Dell = new Computer("Dell", "i7", 8, 1000, true, 750);
+		Computer HP = new Computer("HP", "i5", 16, 500, false, 500);
+		Computer Mac = new Computer("Mac", "Apple Cpu", 12, 1250,true, 1000);
+		Computer Chromebook = new Computer("Chromebook", "Potato", 4, 250, false, 200);
+		System.out.println(Dell);
+		System.out.println(HP);
+		System.out.println(Mac);
+		System.out.println(Chromebook);
+		try(var output =new PrintWriter(new FileOutputStream(
+			    new File("output.txt"), true /* true means append to file */))) {
+			output.println("\nTESTS FOR Computer.java:");
+		//Copy all your lines above, add an extra "2" to every variable name
+		// and replace every System.out.print or System.out.println
+		// by output.print or output.println
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
